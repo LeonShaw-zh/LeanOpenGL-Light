39c86c54f275e6d5a5361594a39495d535db78d4
LeonShaw <LeonShaw.zh@gmail.com>
2018-11-11 10:40:31 +0800

    Multiple lights

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,12 @@ float lastX = screenWidth / 2.0f;
 float lastY = screenHeight / 2.0f;
 bool firstMouse = true;
 // 光源位置
-glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+glm::vec3 pointLightPositions[] = {
+    glm::vec3( 0.7f,  0.2f,  2.0f),
+    glm::vec3( 2.3f, -3.3f, -4.0f),
+    glm::vec3(-4.0f,  2.0f, -12.0f),
+    glm::vec3( 0.0f,  0.0f, -3.0f)
+};
 glm::vec3 cubePositions[] = {
     glm::vec3( 0.0f,  0.0f,  0.0f),
     glm::vec3( 2.0f,  5.0f, -15.0f),
@@ -100,16 +105,54 @@ int main()
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, specularMap);
         colorShader.setFloat("material.shininess", 64.0f);
-        colorShader.setVec3("light.direction", camera.Front);
-        colorShader.setVec3("light.position", camera.Position);
-        colorShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
-        colorShader.setFloat("light.outerCutOff", glm::cos(glm::radians(17.5f)));
-        colorShader.setVec3("light.ambient",  0.2f, 0.2f, 0.2f);
-        colorShader.setVec3("light.diffuse",  0.5f, 0.5f, 0.5f); // 将光照调暗了一些以搭配场景
-        colorShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f); 
-        colorShader.setFloat("light.constant",  1.0f);
-        colorShader.setFloat("light.linear",    0.09f);
-        colorShader.setFloat("light.quadratic", 0.032f);
+        // dir light
+        colorShader.setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
+        colorShader.setVec3("dirLight.ambient", 0.05f, 0.05f, 0.05f);
+        colorShader.setVec3("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
+        colorShader.setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
+        // point light 1
+        colorShader.setVec3("pointLights[0].position", pointLightPositions[0]);
+        colorShader.setVec3("pointLights[0].ambient", 0.05f, 0.05f, 0.05f);
+        colorShader.setVec3("pointLights[0].diffuse", 0.8f, 0.8f, 0.8f);
+        colorShader.setVec3("pointLights[0].specular", 1.0f, 1.0f, 1.0f);
+        colorShader.setFloat("pointLights[0].constant", 1.0f);
+        colorShader.setFloat("pointLights[0].linear", 0.09);
+        colorShader.setFloat("pointLights[0].quadratic", 0.032);
+        // point light 2
+        colorShader.setVec3("pointLights[1].position", pointLightPositions[1]);
+        colorShader.setVec3("pointLights[1].ambient", 0.05f, 0.05f, 0.05f);
+        colorShader.setVec3("pointLights[1].diffuse", 0.8f, 0.8f, 0.8f);
+        colorShader.setVec3("pointLights[1].specular", 1.0f, 1.0f, 1.0f);
+        colorShader.setFloat("pointLights[1].constant", 1.0f);
+        colorShader.setFloat("pointLights[1].linear", 0.09);
+        colorShader.setFloat("pointLights[1].quadratic", 0.032);
+        // point light 3
+        colorShader.setVec3("pointLights[2].position", pointLightPositions[2]);
+        colorShader.setVec3("pointLights[2].ambient", 0.05f, 0.05f, 0.05f);
+        colorShader.setVec3("pointLights[2].diffuse", 0.8f, 0.8f, 0.8f);
+        colorShader.setVec3("pointLights[2].specular", 1.0f, 1.0f, 1.0f);
+        colorShader.setFloat("pointLights[2].constant", 1.0f);
+        colorShader.setFloat("pointLights[2].linear", 0.09);
+        colorShader.setFloat("pointLights[2].quadratic", 0.032);
+        // point light 4
+        colorShader.setVec3("pointLights[3].position", pointLightPositions[3]);
+        colorShader.setVec3("pointLights[3].ambient", 0.05f, 0.05f, 0.05f);
+        colorShader.setVec3("pointLights[3].diffuse", 0.8f, 0.8f, 0.8f);
+        colorShader.setVec3("pointLights[3].specular", 1.0f, 1.0f, 1.0f);
+        colorShader.setFloat("pointLights[3].constant", 1.0f);
+        colorShader.setFloat("pointLights[3].linear", 0.09);
+        colorShader.setFloat("pointLights[3].quadratic", 0.032);
+        // spotLight
+        colorShader.setVec3("spotLight.position", camera.Position);
+        colorShader.setVec3("spotLight.direction", camera.Front);
+        colorShader.setVec3("spotLight.ambient", 0.0f, 0.0f, 0.0f);
+        colorShader.setVec3("spotLight.diffuse", 1.0f, 1.0f, 1.0f);
+        colorShader.setVec3("spotLight.specular", 1.0f, 1.0f, 1.0f);
+        colorShader.setFloat("spotLight.constant", 1.0f);
+        colorShader.setFloat("spotLight.linear", 0.09);
+        colorShader.setFloat("spotLight.quadratic", 0.032);
+        colorShader.setFloat("spotLight.cutOff", glm::cos(glm::radians(12.5f)));
+        colorShader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(15.0f)));
         // 顶点着色器需要的参数
         colorShader.setMat4("view", view);
         colorShader.setMat4("projection", projection);
@@ -129,14 +172,16 @@ int main()
         // 绘制光源
         lampShader.use();
         // 顶点着色器需要的参数
-        model = glm::mat4(1.0f);
-        model = glm::translate(model, lightPos);
-        model = glm::scale(model, glm::vec3(0.2f));
-        lampShader.setMat4("model", model);
         lampShader.setMat4("view", view);
         lampShader.setMat4("projection", projection);
-        glBindVertexArray(VAOLight);
-        // glDrawArrays(GL_TRIANGLES, 0, 36);
+        for(unsigned int i = 0; i < 4; i++){
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, pointLightPositions[i]);
+            model = glm::scale(model, glm::vec3(0.2f));
+            lampShader.setMat4("model", model);
+            glBindVertexArray(VAOLight);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         // 应用程序采用着双缓冲模式，一张在前面显示，一张在后面慢慢加载
         // Swap交换缓冲，完成立刻刷新
